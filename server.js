93087dd985957adf75894b3307d724a4b724749e
Joe Waine <joe.waine@gmail.com>
2017-11-08 13:44:37 -0800

    tuesday lab turn in

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
+
 client.connect();
 client.on('error', err => console.error(err));
 app.use(cors());
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+
+
+app.get('/about', (req, res) => res.send('about page'));
 
 
 app.get('/test', (req, res) => res.send('Testing test1, 2, 3'));
@@ -22,6 +26,21 @@ app.get('/api/v1/books', (req, res) => {
     .then(results => res.send(results.rows))
     .catch(console.log('changes'));
 });
+
+app.get('/api/v1/books/:id', (req, res) => {
+    client.query(`SELECT * FROM books
+    WHERE book_id=$1;`,[1])
+    .then(results => res.send(results.rows))
+    .catch(console.log('changes'));
+
+});
+
+
+
+
+
+
+
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
