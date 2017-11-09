2a1a701730fe55927316d9b0a3738fee9610e306
Joe Waine <joe.waine@gmail.com>
2017-11-08 18:27:23 -0800

    server fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,11 +15,7 @@ client.on('error', err => console.error(err));
 app.use(cors());
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
-
-
 app.get('/about', (req, res) => res.send('about page'));
-
-
 app.get('/test', (req, res) => res.send('Testing test1, 2, 3'));
 
 app.get('/api/v1/books', (req, res) => {
@@ -29,35 +26,27 @@ app.get('/api/v1/books', (req, res) => {
 
 app.get('/api/v1/books/:id', (req, res) => {
     client.query(`SELECT * FROM books
-    WHERE book_id=$1;`,[req.params.id])
+    WHERE book_id=${req.params.id};`)
     .then(results => res.send(results.rows))
     .catch(console.log('changes'));
 });
 
 
-
-app.post('/api/v1/books/', (req, res) => {
+app.post('/api/v1/books/', bodyParser, (req, res) => {
   let {title, author, isbn, image_url, description} = req.body;
     client.query(`INSERT INTO books(title, author, isbn, image_url, description)
     VALUES($1,$2,$3,$4,$5,$6)`,[title, author, isbn, image_url, description])
     .then(results => res.sendStatus(201))
-    .catch(console.log('changes'));
+    .catch(console.error);
 });
 
 
-
-
-
-
-
-
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
-
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-function loadDB() {
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    book(id serial primary key, title varchar(255), description varchar(255), author varchar(255), image_url varchar(255));
-    `)
-}
+// function loadDB() {
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     book(id serial primary key, title varchar(255), description varchar(255), author varchar(255), image_url varchar(255));
+//     `)
+// }
